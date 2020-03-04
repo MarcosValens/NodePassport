2d704b2eae4314fe32836baf305c22cc0bfe8b30
Marcos <marcosvalens@gmail.com>
2020-03-04 17:11:34 +0100

    Server Routes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
-
+const authRoutes = require('./routes/auth-routes')
 const app = express();
 
-//set up viw engine
+// set up viw engine
 app.set('view engine','ejs');
 
-// create home route
+// set up routes
+app.use('/auth',authRoutes);
 
+// create home route
 app.get('/',(req,res) =>{
     res.render('home')
 });
