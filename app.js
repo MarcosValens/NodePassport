8a284d050561499c08e659ad332fb77d92d24efb
marcos <marcosvalens@gmail.com>
2020-03-05 06:14:18 +0100

    finished

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const app = express();
 
 // set up viw engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
+
+// set up cookie-session
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000, //1 day
+    keys: [keys.session.cookieKey]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // connect to mongoDB
-mongoose.connect(keys.mongoDB.dbURI, {useNewUrlParser: true, useUnifiedTopology: true},()  =>{
+mongoose.connect(keys.mongoDB.dbURI, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
     console.log('Connected to DB')
 });
 
 // set up routes
-app.use('/auth',authRoutes);
+app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 
 // create home route
-app.get('/',(req,res) =>{
-    res.render('home')
+app.get('/', (req, res) => {
+    res.render('home', {user: res.user})
 });
 
-app.listen(9999,() =>{
-console.log('Listen port 9999')
+app.listen(9999, () => {
+    console.log('Listen port 9999')
 });
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,15 +1,39 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../model/user-model');
+
+passport.serializeUser((user,done) =>{
+    done(null, user.id)
+});
+
+passport.deserializeUser(async (id,done) =>{
+    let receivedUser = await User.findById(id);
+    done(null, receivedUser)
+});
 
 passport.use(new GoogleStrategy({
         //options for strategy
-        callbackURL:'/auth/google/redirect',
-        clientID:keys.google.clientID,
-        clientSecret:keys.google.clientSecret
-    }, (accesToken, refreshToken, profile, done) => {
+        callbackURL: '/auth/google/redirect',
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
+    }, async (accesToken, refreshToken, profile, done) => {
         // passport callback function
-        console.log('passport callbacn function fired')
-        console.log(profile)
+        // check if user already exists in our db
+        let currentUser = await User.findOne({
+            googleId: profile.id
+        });
+        if (currentUser) {
+            // already have the user
+            done(null, currentUser)
+        } else {
+            //if not, create user in our db
+            let newUser = await new User({
+                username: profile.displayName,
+                googleId: profile.id,
+                thumbnail:profile._json.picture,
+            }).save();
+            done(null, newUser)
+        }
     })
 );
