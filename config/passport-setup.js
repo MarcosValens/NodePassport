0c51181487d77bc447392094d17c33f588034be6
marcos <marcosvalens@gmail.com>
2020-03-05 03:32:38 +0100

    Google Redirect working

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,8 +4,10 @@ const keys = require('./keys');
 
 passport.use(new GoogleStrategy({
         //options for strategy
+        callbackURL:'/auth/google/redirect',
         clientID:keys.google.clientID,
         clientSecret:keys.google.clientSecret
-    }), () => {
+    }, () => {
         // passport callback function
-    });
+    })
+);
